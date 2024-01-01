e26ec97ca3b5e1ad893afddf3e1d9b69ef707e07
MjFarzaneh <m.farzane@ernyka.com>
2024-01-01 10:54:29 +0330

    First Commit on master Branch

---
+++ src/main/java/com/training/kafka/TrainingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.training.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingKafkaApplication.class, args);
+	}
+
+}
